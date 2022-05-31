eece57013dfaec42368ca0dd103f4d07883fb857
Jhon Arlex Ocampo <jhon.ocampo123@gmail.com>
2022-05-31 10:42:06 -0500

    ajuste environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   graphqlUri: 'https://asvida-subscription-server-4dlk3.ondigitalocean.app/graphql',
+  server: 'https://asvida-subscription-server-4dlk3.ondigitalocean.app',
 };
